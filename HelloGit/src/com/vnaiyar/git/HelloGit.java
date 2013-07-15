e37ee9491f6f7dd1e20ab27f69bf5d4e0c116b1c
Ricky22 <vikasnaiyar@gmail.com>
2013-07-15 09:29:17 +0530

    HelloCommit

---
+++ HelloGit/src/com/vnaiyar/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.vnaiyar.git;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
